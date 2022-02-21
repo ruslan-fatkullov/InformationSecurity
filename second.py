63ce92c25452b5aa241b0a63aa2a455033dad856
ruslan-fatkullov <fatkullov1999@gmail.com>
2022-02-21 20:22:34 +0400

    change second lab

--- second.py
+++ second.py
@@ -1,33 +1,39 @@
 import time
 import first
+import math
+
+
+def frequency_test(string_for_check):
+    for i in range(len(string_for_check)):
+        string_for_check[i] = 2 * int(string_for_check[i]) - 1
+    return abs(sum(string_for_check)) / math.sqrt(len(string_for_check)) <= 1.82138636
 
 
 def generic_symbol(x):
-    x = ((16807 * x) % 2147483647)-1
-    return int(x)
+    temp = ((16807 * x) % 2147483647)-1
+    return int(temp)
 
 
 def init():
     print("Вторая лабораторная работа")
     print("введите длину последовательности")
     len_of_string = input()
-    result_list_of_numbers = list()
+
     temp = int(time.time())
+    result_string = ""
     for _ in range(int(len_of_string)):
         temp = generic_symbol(temp)
-        result_list_of_numbers.append(format(temp, 'b'))
-
-    result_string = ""
-    for i in result_list_of_numbers:
-        result_string += i
-
-    file2 = open("source/second.txt", "w")
-    file2.write(result_string)
-    file2.close()
+        res = (temp % 2)
+        # print(res)
+        result_string += str(res)
 
     check_list = list(result_string)
-    x: str = "Частотный тест пройден" if first.frequency_test(check_list) else "Частотный тест не пройден"
+    x: str = "Частотный тест пройден" if frequency_test(check_list) else "Частотный тест не пройден"
     print(x)
     y: str = "Тест на последовательность одинаковых бит пройден" if first.sequence_identical_bits(
         check_list) else "Тест на последовательность одинаковых бит не пройден"
     print(y)
+
+    file2 = open("source/second.txt", "w")
+    file2.write(result_string)
+    file2.close()
